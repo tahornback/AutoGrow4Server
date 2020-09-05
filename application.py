0a072d285e44178c8e40cbb5f77bd89b382d96db
Tim Hornback <tim.hornback@my.simpson.edu>
2020-09-05 15:01:26 -0500

    Make it nice for EB

---
+++ application.py
@@ -0,0 +1,11 @@
+from flask import Flask
+application = Flask(__name__)
+
+
+@application.route('/')
+def hello():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    application.debug = True
+    application.run()
