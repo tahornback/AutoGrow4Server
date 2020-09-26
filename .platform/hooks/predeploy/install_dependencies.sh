a9f18115d1cb151b3362e10ee4e1e8b660b31286
Tim Hornback <tim.hornback@my.simpson.edu>
2020-09-26 17:15:26 -0500

    remove all the install dependencies to see if eb passes

--- .platform/hooks/predeploy/install_dependencies.sh
+++ .platform/hooks/predeploy/install_dependencies.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-sudo yum install openbabel
-obabel_path=$(which openbabel)
-sudo yum install jq
-jsonStr=$(cat ./autogrow4-4.0.2/sample_submit_autogrow.json)
-jq 'del(.mgltools_directory)' <<<"$jsonStr"
-temp='. + { "obabel_path": "'
-temp+=obabel_path
-temp+='" }'
-jq temp <<<"$jsonStr" > tmp.$$.json && mv tmp.$$.json ./autogrow4-4.0.2/sample_submit_autogrow.json
+#wget http://mgltools.scripps.edu/downloads/downloads/tars/releases/REL1.5.6/mgltools_x86_64Linux2_1.5.6.tar.gz
+#tar -xvzf mgltools_x86_64Linux2_1.5.6.tar.gz
+#cd mgltools_x86_64Linux2_1.5.6
+#./install.sh
+#mgltools_directory=$(which mgltools)
+#sudo yum install jq
+#jsonStr=$(cat ./autogrow4-4.0.2/sample_submit_autogrow.json)
+#jq 'del(.mgltools_directory)' <<<"$jsonStr"
+#temp='. + { "mgltools_directory": "'
+#temp+=mgltools_directory
+#temp+='" }'
+#jq temp <<<"$jsonStr" > tmp.$$.json && mv tmp.$$.json ./autogrow4-4.0.2/sample_submit_autogrow.json
