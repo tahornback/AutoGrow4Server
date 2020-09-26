86197963000c4c65d558c97605f2ce79c7449e68
Tim Hornback <tim.hornback@my.simpson.edu>
2020-09-26 18:46:05 -0500

    change path

--- .platform/hooks/predeploy/install_dependencies.sh
+++ .platform/hooks/predeploy/install_dependencies.sh
@@ -3,7 +3,6 @@ wget http://mgltools.scripps.edu/downloads/downloads/tars/releases/REL1.5.6/mglt
 tar -xvzf mgltools_x86_64Linux2_1.5.6.tar.gz
 cd mgltools_x86_64Linux2_1.5.6
 ./install.sh
-echo $(which mgltools)
 #mgltools_directory=$(which mgltools)
 #sudo yum install jq
 #jsonStr=$(cat ./autogrow4-4.0.2/sample_submit_autogrow.json)
