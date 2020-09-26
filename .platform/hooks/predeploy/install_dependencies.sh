834b13b43777dc2412922c3eaa18d8393f534d1b
Tim Hornback <tim.hornback@my.simpson.edu>
2020-09-26 17:24:41 -0500

    do mgltools stuff

--- .platform/hooks/predeploy/install_dependencies.sh
+++ .platform/hooks/predeploy/install_dependencies.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#wget http://mgltools.scripps.edu/downloads/downloads/tars/releases/REL1.5.6/mgltools_x86_64Linux2_1.5.6.tar.gz
-#tar -xvzf mgltools_x86_64Linux2_1.5.6.tar.gz
-#cd mgltools_x86_64Linux2_1.5.6
-#./install.sh
+wget http://mgltools.scripps.edu/downloads/downloads/tars/releases/REL1.5.6/mgltools_x86_64Linux2_1.5.6.tar.gz
+tar -xvzf mgltools_x86_64Linux2_1.5.6.tar.gz
+cd mgltools_x86_64Linux2_1.5.6
+./install.sh
 #mgltools_directory=$(which mgltools)
 #sudo yum install jq
 #jsonStr=$(cat ./autogrow4-4.0.2/sample_submit_autogrow.json)
