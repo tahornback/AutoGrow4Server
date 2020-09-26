64bb9f654ba64ccd4986d53f726d88b94f080310
Tim Hornback <tim.hornback@my.simpson.edu>
2020-09-26 17:03:44 -0500

    try mgltools

--- .platform/hooks/predeploy/install_dependencies.sh
+++ .platform/hooks/predeploy/install_dependencies.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-sudo yum install openbabel
-obabel_path=$(which openbabel)
+wget http://mgltools.scripps.edu/downloads/downloads/tars/releases/REL1.5.6/mgltools_x86_64Linux2_1.5.6.tar.gz
+tar -xvzf mgltools_x86_64Linux2_1.5.6.tar.gz
+cd mgltools_x86_64Linux2_1.5.6
+./install.sh
+mgltools_directory=$(which mgltools)
 sudo yum install jq
 jsonStr=$(cat ./autogrow4-4.0.2/sample_submit_autogrow.json)
 jq 'del(.mgltools_directory)' <<<"$jsonStr"
-temp='. + { "obabel_path": "'
-temp+=obabel_path
+temp='. + { "mgltools_directory": "'
+temp+=mgltools_directory
 temp+='" }'
 jq temp <<<"$jsonStr" > tmp.$$.json && mv tmp.$$.json ./autogrow4-4.0.2/sample_submit_autogrow.json
