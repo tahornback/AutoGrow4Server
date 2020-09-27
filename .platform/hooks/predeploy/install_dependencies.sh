a221cf05621f2a24932295f58ead3bfcb3cf5295
Tim Hornback <tim.hornback@my.simpson.edu>
2020-09-27 15:44:15 -0500

    dir

--- .platform/hooks/predeploy/install_dependencies.sh
+++ .platform/hooks/predeploy/install_dependencies.sh
@@ -12,6 +12,7 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x "./Miniconda3-latest-Linux-x86_64.sh"
 ./Miniconda3-latest-Linux-x86_64.sh -b -p /conda
 cd conda
+dir
 conda list
 conda install -c conda-forge rdkit
 conda install numpy
