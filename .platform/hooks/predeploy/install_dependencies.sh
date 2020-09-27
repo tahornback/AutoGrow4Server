dfb4f9b1287a666585c22ee19c74f97638251cc2
Tim Hornback <tim.hornback@my.simpson.edu>
2020-09-27 15:26:24 -0500

    why did this stop working

--- .platform/hooks/predeploy/install_dependencies.sh
+++ .platform/hooks/predeploy/install_dependencies.sh
@@ -2,15 +2,15 @@
 #sudo yum install -y epel-release
 #sudo yum install openbabel
 cd /
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-chmod +x "./Miniconda3-latest-Linux-x86_64.sh"
-./Miniconda3-latest-Linux-x86_64.sh -b -p /conda
-cd /
 wget http://mgltools.scripps.edu/downloads/downloads/tars/releases/REL1.5.6/mgltools_x86_64Linux2_1.5.6.tar.gz
 tar -xvzf mgltools_x86_64Linux2_1.5.6.tar.gz
 cd mgltools_x86_64Linux2_1.5.6
 ./install.sh
 source ./initMGLtools.sh
+cd /
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod +x "./Miniconda3-latest-Linux-x86_64.sh"
+./Miniconda3-latest-Linux-x86_64.sh -b -p /conda
 conda install -c conda-forge rdkit
 conda install numpy
 conda install scipy
