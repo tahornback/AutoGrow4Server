18fea74060f481276fb9fa3ba4fd33b5af595757
Tim Hornback <tim.hornback@my.simpson.edu>
2020-09-27 15:03:18 -0500

    get everything in one script

--- .platform/hooks/predeploy/install_dependencies.sh
+++ .platform/hooks/predeploy/install_dependencies.sh
@@ -2,11 +2,17 @@
 #sudo yum install -y epel-release
 #sudo yum install openbabel
 cd /
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+./Miniconda3-latest-Linux-x86_64.sh -b -p /
+cd /
 wget http://mgltools.scripps.edu/downloads/downloads/tars/releases/REL1.5.6/mgltools_x86_64Linux2_1.5.6.tar.gz
 tar -xvzf mgltools_x86_64Linux2_1.5.6.tar.gz
 cd mgltools_x86_64Linux2_1.5.6
 ./install.sh
 source ./initMGLtools.sh
+conda install -c conda-forge rdkit
+conda install numpy
+conda install scipy
 #mgltools_directory=$(which mgltools)
 #sudo yum install jq
 #jsonStr=$(cat ./autogrow4-4.0.2/sample_submit_autogrow.json)
--- .platform/hooks/predeploy/python_conda_installs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-conda install -c conda-forge rdkit
-conda install numpy
-conda install scipy
