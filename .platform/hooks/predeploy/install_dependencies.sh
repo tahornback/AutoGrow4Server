d785c72032ff6304cc41cf0491c43aa74b82ba38
Tim Hornback <tim.hornback@my.simpson.edu>
2020-09-27 20:35:44 -0500

    why are you stealing from dockerfile

--- .platform/hooks/predeploy/install_dependencies.sh
+++ .platform/hooks/predeploy/install_dependencies.sh
@@ -15,15 +15,16 @@ if [ ! -f "Miniconda3-latest-Linux-x86_64.sh" ]; then
     echo "Miniconda3-latest-Linux-x86_64.sh does not exist."
     wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
     chmod +x "./Miniconda3-latest-Linux-x86_64.sh"
-    ./Miniconda3-latest-Linux-x86_64.sh -b -p /conda
 fi
+./Miniconda3-latest-Linux-x86_64.sh -b
 echo "conda before"
 conda list
-if [ ! $? -eq 0 ];
+if [ ! 1 -eq 0 ];
 then
-    echo "adding path to bashrc"
-    export PATH=/conda/bin:$PATH
-    "export PATH=/conda/bin:$PATH" >> ~/.bashrc
+    echo "adding aliases to bashrc"
+    echo "alias python=/root/miniconda3/bin/python" >> /root/.bashrc
+    echo "alias conda=/root/miniconda3/bin/conda" >> /root/.bashrc
+    echo "alias pip=/root/miniconda3/bin/pip" >> /root/.bashrc
     echo "sourcing bashrc"
     source ~/.bashrc
 fi
@@ -40,9 +41,11 @@ cat ~/.bashrc
 echo "bash_profile"
 cat ~/.bash_profile
 conda list
-conda install -c conda-forge rdkit
-conda install numpy
-conda install scipy
+/root/miniconda3/bin/conda install -y -c conda-forge rdkit rdkit=2020.03.1
+/root/miniconda3/bin/conda install -y numpy numpy=1.18.1
+/root/miniconda3/bin/conda install -y scipy scipy=1.4.1
+/root/miniconda3/bin/pip install matplotlib==3.2.1
+/root/miniconda3/bin/pip install func_timeout==4.3.5
 #mgltools_directory=$(which mgltools)
 #sudo yum install jq
 #jsonStr=$(cat ./autogrow4-4.0.2/sample_submit_autogrow.json)
--- application.py
+++ application.py
@@ -12,7 +12,7 @@ def execute():
     # py_file = "{}{}autogrow4-4.0.2{}RunAutogrow.py".format(os.getcwd(), os.sep, os.sep)
     # params = "{}{}autogrow4-4.0.2{}sample_sub_scripts{}sample_submit_autogrow.json".format(os.getcwd(), os.sep, os.sep, os.sep)
     py_file = "RunAutogrow.py"
-    params = "sample_sub_scripts{}sample_submit_autogrow.json".format(os.sep)
+    params = "./sample_sub_scripts{}sample_submit_autogrow.json".format(os.sep)
     os.chdir("autogrow4-4.0.2")
     command = "python {} -j {}".format(py_file, params)
     os.system(command)
