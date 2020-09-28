320ed03ab9e504e44569ffbadaf51545a2dd5499
Tim Hornback <tim.hornback@my.simpson.edu>
2020-09-27 20:49:55 -0500

    back to how it was

--- .platform/hooks/predeploy/install_dependencies.sh
+++ .platform/hooks/predeploy/install_dependencies.sh
@@ -15,19 +15,23 @@ if [ ! -f "Miniconda3-latest-Linux-x86_64.sh" ]; then
     echo "Miniconda3-latest-Linux-x86_64.sh does not exist."
     wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
     chmod +x "./Miniconda3-latest-Linux-x86_64.sh"
+    ./Miniconda3-latest-Linux-x86_64.sh -b -p /conda
 fi
-./Miniconda3-latest-Linux-x86_64.sh -b
 echo "conda before"
 conda list
 if [ ! $? -eq 0 ];
 then
-    echo "adding aliases to bashrc"
-    echo "alias python=/root/miniconda3/bin/python" >> /root/.bashrc
-    echo "alias conda=/root/miniconda3/bin/conda" >> /root/.bashrc
-    echo "alias pip=/root/miniconda3/bin/pip" >> /root/.bashrc
+    echo "adding path to bashrc"
+    export PATH=/conda/bin:$PATH
+    echo "export PATH=/conda/bin:$PATH" >> /root/.bashrc
+    echo "alias python=/conda/bin/python" >> /root/.bashrc
+    echo "alias conda=/conda/bin/conda" >> /root/.bashrc
+    echo "alias pip=/conda/bin/pip" >> /root/.bashrc
     echo "sourcing bashrc"
     source ~/.bashrc
 fi
+echo "conda after"
+conda list
 #echo "ls root"
 #ls /
 #echo "ls /conda"
@@ -39,11 +43,9 @@ cat ~/.bashrc
 echo "bash_profile"
 cat ~/.bash_profile
 conda list
-/root/miniconda3/bin/conda install -y -c conda-forge rdkit rdkit=2020.03.1
-/root/miniconda3/bin/conda install -y numpy numpy=1.18.1
-/root/miniconda3/bin/conda install -y scipy scipy=1.4.1
-/root/miniconda3/bin/pip install matplotlib==3.2.1
-/root/miniconda3/bin/pip install func_timeout==4.3.5
+conda install -c conda-forge rdkit
+conda install numpy
+conda install scipy
 conda list
 #mgltools_directory=$(which mgltools)
 #sudo yum install jq
