820001bf1df1646036d74a4cb8330000edbc3cee
Tim Hornback <tim.hornback@my.simpson.edu>
2020-10-07 15:13:04 -0500

    Pull out some logic from main execute

--- autogrow4-4.0.2/autogrow/autogrow_main_execute.py
+++ autogrow4-4.0.2/autogrow/autogrow_main_execute.py
@@ -29,18 +29,7 @@ def main_execute(vars):
     num_gens_to_make = vars["num_generations"]
 
     # Determine what was the last completed generation in the Run directory
-    last_generation = determine_current_gen(output_directory)
-    if last_generation is None:
-        # Check to see if there's a Run 0 based on the seed.
-        if vars["use_docked_source_compounds"] is True:
-            # This will assess and rank the source compounds prior to
-            # generation 1. Thus using the source compounds as a generation 0
-            starting_generation_num = 0
-        else:
-            starting_generation_num = 1
-
-    else:
-        starting_generation_num = last_generation + 1
+    starting_generation_num = current_generation_number = 0
 
     if starting_generation_num > num_gens_to_make:
         print("This simulation has already been completed to the user defined number \
@@ -56,88 +45,51 @@ def main_execute(vars):
     # 2)  Docking which handles converting from PDBs to Docking specific
     #     formats and running the actual Docking simulations
     # 3)  Ranking the generation based on the Docking scores
-    for current_generation_number in range(starting_generation_num, num_gens_to_make+1):
-        sys.stdout.flush()
+    sys.stdout.flush()
 
-        # Get directory for smi to go
-        current_generation_dir = vars["output_directory"] + "generation_{}{}".format(current_generation_number, os.sep)
-        print(current_generation_dir)
-        sys.stdout.flush()
+    # Get directory for smi to go
+    current_generation_dir = vars["output_directory"] + "generation_{}{}".format(current_generation_number, os.sep)
+    print(current_generation_dir)
+    sys.stdout.flush()
 
-        if current_generation_number == 0 and vars["use_docked_source_compounds"] is True:
-            if os.path.exists(current_generation_dir + os.sep + "generation_0_ranked.smi") is True:
+    already_docked, smile_file_new_gen, new_gen_ligands_list = operations.populate_generation_zero(vars, generation_num=0)
+    sys.stdout.flush()
+
+    # Run file conversions of PDB to docking specific file type
+    # and Begin Docking unweighted_ranked_smile_file is the file
+    # name where the unweighted ranked but score .smi file resides
+    unweighted_ranked_smile_file = DockingClass.run_docking_common(
+        vars, current_generation_number,
+        current_generation_dir, smile_file_new_gen)
+
+    # Delete all temporary files; Skip if in Debugging Mode
+    if vars["debug_mode"] is False:
+        print("Deleting temporary files and directories")
+        files_to_del = []
+        folders_to_del = ["{}{}3D_SDFs{}".format(current_generation_dir, os.sep, os.sep), "{}{}3D_SDFs{}log{}".format(current_generation_dir, os.sep, os.sep, os.sep), "{}{}gypsum_submission_files{}".format(current_generation_dir, os.sep, os.sep)]
+        for folder in folders_to_del:
+            if os.path.exists(folder) is False:
                 continue
+            files_to_del.extend(glob.glob(folder+"*"))
 
-            already_docked, smile_file_new_gen, new_gen_ligands_list = operations.populate_generation_zero(vars, generation_num=0)
-            sys.stdout.flush()
+        job_input = tuple([tuple([x]) for x in files_to_del if os.path.isfile(x) is True])
+        vars["parallelizer"].run(job_input, delete_temporary_files_and_folders)
+        # Delete Folders in an ordered manor incase folders are nested
+        for i in range(0, len(folders_to_del)):
+            delete_temporary_files_and_folders(folders_to_del[i])
 
-            if already_docked is False:
-                # Run file conversions of PDB to docking specific file type
-                # and Begin Docking unweighted_ranked_smile_file is the file
-                # name where the unweighted ranked but score .smi file resides
-                unweighted_ranked_smile_file = DockingClass.run_docking_common(
-                    vars, current_generation_number,
-                    current_generation_dir, smile_file_new_gen)
-
+    sys.stdout.flush()
+    if vars["reduce_files_sizes"] is True:
+        # Reduce the files in the PDBs folder to a single compiled file.
+        # This reduces the data size And makes it easier to transfer the
+        # data
+        pdbs_folder = "{}{}PDBs{}".format(current_generation_dir, os.sep, os.sep)
+        if os.path.exists(pdbs_folder) is True:
+            concatenate_files.run_concatenation(vars["parallelizer"], pdbs_folder)
         else:
-            smile_file_new_gen, new_gen_ligands_list = operations.populate_generation(vars, current_generation_number)
-            sys.stdout.flush()
-
-            if new_gen_ligands_list is None:
-                raise ValueError("Population failed to make enough mutants or crossovers... \
-                                    Errors could include not enough diversity, too few seeds to the generation, \
-                                    the seed mols are unable to cross-over due to lack of similarity,\
-                                    or all of the seed lack functional groups for performing reactions.")
-
-            # Run file conversions of PDB to docking specific file type and
-            # Begin Docking unweighted_ranked_smile_file is the file name
-            # where the unweighted ranked but score .smi file resides
-            unweighted_ranked_smile_file = DockingClass.run_docking_common(vars, current_generation_number, current_generation_dir, smile_file_new_gen)
-
-        # Delete all temporary files; Skip if in Debugging Mode
-        if vars["debug_mode"] is False:
-            print("Deleting temporary files and directories")
-            files_to_del = []
-            folders_to_del = ["{}{}3D_SDFs{}".format(current_generation_dir, os.sep, os.sep), "{}{}3D_SDFs{}log{}".format(current_generation_dir, os.sep, os.sep, os.sep), "{}{}gypsum_submission_files{}".format(current_generation_dir, os.sep, os.sep)]
-            for folder in folders_to_del:
-                if os.path.exists(folder) is False:
-                    continue
-                files_to_del.extend(glob.glob(folder+"*"))
-
-            job_input = tuple([tuple([x]) for x in files_to_del if os.path.isfile(x) is True])
-            vars["parallelizer"].run(job_input, delete_temporary_files_and_folders)
-            # Delete Folders in an ordered manor incase folders are nested
-            for i in range(0, len(folders_to_del)):
-                delete_temporary_files_and_folders(folders_to_del[i])
-
-        sys.stdout.flush()
-        if vars["reduce_files_sizes"] is True:
-            # Reduce the files in the PDBs folder to a single compiled file.
-            # This reduces the data size And makes it easier to transfer the
-            # data
-            pdbs_folder = "{}{}PDBs{}".format(current_generation_dir, os.sep, os.sep)
-            if os.path.exists(pdbs_folder) is True:
-                concatenate_files.run_concatenation(vars["parallelizer"], pdbs_folder)
-            else:
-                print("\nNo PDB folder to concatenate and compress. This is likely generation 0 seeded with a Ranked .smi file.\n")
-        print("")
-        print("Finished generation ", current_generation_number)
-
-        sys.stdout.flush()
-
-    if vars["generate_plot"] is True:
-        matplotlib_is_callable = False
-        try:
-            import matplotlib
-            matplotlib_is_callable = True
-        except:
-            matplotlib_is_callable = False
-        if matplotlib_is_callable is False:
-            print("Can not make figure as matplotlib is not installed")
-        else:
-            print("Plotting")
-            import autogrow.plotting.generate_line_plot as plot
-            plot.generate_figures(vars)
+            print("\nNo PDB folder to concatenate and compress. This is likely generation 0 seeded with a Ranked .smi file.\n")
+    print("")
+    print("Finished generation ", current_generation_number)
 
     sys.stdout.flush()
 #
