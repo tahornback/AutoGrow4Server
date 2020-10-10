4d98b3066114fe5d48cb028071b72c80f51e5ff8
Tim Hornback <tim.hornback@my.simpson.edu>
2020-10-09 22:38:02 -0500

    fix some of the time trial stuff

--- application.py
+++ application.py
@@ -1,4 +1,6 @@
-from flask import Flask
+import time
+
+from flask import Flask, request
 import os
 
 application = Flask(__name__)
@@ -14,25 +16,28 @@ def execute():
     py_file = "RunAutogrow.py"
     params = "./sample_sub_scripts{}sample_submit_autogrow.json".format(os.sep)
     output_file = "current_run_output.txt"
-    tree_file = "tree.txt"
     os.chdir("autogrow4-4.0.2")
     command = "/conda/bin/python {} -j {}".format(py_file, params)
     os.system("{} > {}".format(command, output_file))
-    os.chdir("Outputfolder")
-    os.system("du -a > {}".format(tree_file))
-    os.chdir("../..")
-    return "command: {}<br/>json file: {}<br/>output:<br/>{}<br/>tree:<br/>{}<br/>docking output:<br/>{}".format(
+    os.chdir("..")
+    return "command: {}<br/>json file: {}<br/>output:<br/>{}".format(
         command,
         open("autogrow4-4.0.2/" + params).read(),
         open("autogrow4-4.0.2/" + output_file).read().replace(os.linesep, "<br/>"),
-        open("autogrow4-4.0.2/Outputfolder/" + tree_file)
-        .read()
-        .replace(os.linesep, "<br/>"),
-        open(
-            "autogrow4-4.0.2/Outputfolder/Run_0/generation_0/PDBs/naphthalene_22__1.pdbqt_docking_output.txt"
-        )
-        .read()
-        .replace(os.linesep, "<br/>"),
+    )
+
+
+@application.route("/execute-time-trial")
+def executeTimeTrial():
+    start_time = time.time()
+    times_to_execute = request.args.get("times")
+    printout = ""
+    for i in range(int(times_to_execute)):
+        printout += execute() + "<br/>"
+    end_time = time.time()
+    time_taken = end_time - start_time
+    return "{} executions finished in {} seconds, averaging {} seconds per run<br/><br/><br/>{}".format(
+        times_to_execute, str(time_taken), str(time_taken / times_to_execute), printout
     )
 
 
