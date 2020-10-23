44be1b44fc969f4327140b4c4540685fc5b334e9
Tim Hornback <tim.hornback@my.simpson.edu>
2020-10-22 22:41:17 -0500

    drop conditional

--- application.py
+++ application.py
@@ -1,5 +1,2 @@
 import os
-if __name__ == "__main__":
-    os.system("/conda/bin/python ./flask_app.py")
-else:
-    raise Exception("application.py not started as __main__")
+os.system("/conda/bin/python ./flask_app.py")
