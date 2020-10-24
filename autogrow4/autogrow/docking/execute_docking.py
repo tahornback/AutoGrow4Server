7efd6dc50798daa7f8da2b811e687e6f10d2b097
Tim Hornback <tim.hornback@my.simpson.edu>
2020-10-24 11:31:40 -0500

    not done fixing imports yet 2

--- autogrow4/autogrow/docking/execute_docking.py
+++ autogrow4/autogrow/docking/execute_docking.py
@@ -4,18 +4,30 @@ This script handles the docking and file conversion for docking.
 import __future__
 
 import os
+try:
+    from autogrow4.autogrow.docking.docking_class.get_child_class import get_all_subclasses
 
-from autogrow4.autogrow.docking.docking_class.get_child_class import get_all_subclasses
+    from autogrow4.autogrow.docking.docking_class.docking_class_children import *
+    from autogrow4.autogrow.docking.docking_class.parent_dock_class import ParentDocking
+    # from autogrow4.autogrow.docking.docking_class.docking_class_children \
+    #                           import VinaDocking, QuickVina2Docking
 
-from autogrow4.autogrow.docking.docking_class.docking_class_children import *
-from autogrow4.autogrow.docking.docking_class.parent_dock_class import ParentDocking
-# from autogrow4.autogrow.docking.docking_class.docking_class_children \
-#                           import VinaDocking, QuickVina2Docking
+    from autogrow4.autogrow.docking.docking_class.docking_file_conversion import *
+    from autogrow4.autogrow.docking.docking_class.parent_pdbqt_converter import ParentPDBQTConverter
+    # from autogrow4.autogrow.docking.docking_class.docking_file_conversion \
+    #                           import convert_with_obabel, convert_with_mgltools
+except Exception as e:
+    from autogrow4.autogrow.docking.docking_class.get_child_class import get_all_subclasses
 
-from autogrow4.autogrow.docking.docking_class.docking_file_conversion import *
-from autogrow4.autogrow.docking.docking_class.parent_pdbqt_converter import ParentPDBQTConverter
-# from autogrow4.autogrow.docking.docking_class.docking_file_conversion \
-#                           import convert_with_obabel, convert_with_mgltools
+    from autogrow4.autogrow.docking.docking_class.docking_class_children import *
+    from autogrow4.autogrow.docking.docking_class.parent_dock_class import ParentDocking
+    # from autogrow4.autogrow.docking.docking_class.docking_class_children \
+    #                           import VinaDocking, QuickVina2Docking
+
+    from autogrow4.autogrow.docking.docking_class.docking_file_conversion import *
+    from autogrow4.autogrow.docking.docking_class.parent_pdbqt_converter import ParentPDBQTConverter
+    # from autogrow4.autogrow.docking.docking_class.docking_file_conversion \
+    #                           import convert_with_obabel, convert_with_mgltools
 
 
 def pick_docking_class_dict(dock_choice):
