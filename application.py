97b174a1892d1450eefa49d3ec8851f115fa0d43
Tim Hornback <tim.hornback@my.simpson.edu>
2020-10-28 15:10:28 -0500

    cut out timing stuff

--- application.py
+++ application.py
@@ -46,35 +46,22 @@ def updateProperties():
     #     heavyAtoms: { INTEGER },
     #     complexity: { DECIMAL }
     # }
-    total_start = time.time()
-    output = ""
-    start = time.time()
     if not os.path.exists(os.getcwd()+'/smiles_dir'):
         os.makedirs(os.getcwd()+'/smiles_dir')
     if not os.path.exists(os.getcwd()+'/smiles_dir/generation_0'):
         os.makedirs(os.getcwd()+'/smiles_dir/generation_0')
-    end = time.time()
-    output += "directory making: {} ".format(str(end-start))
-    start = time.time()
     json = request.get_json()
-    #{game: 1, mol: "asdfas"}
     mol2d = json.get("mol")
     referred_mol_name = "".join(random.choice(string.ascii_letters) for i in range(8))
     temp_file_name = referred_mol_name.join(".sdf")
     temp_file = open(temp_file_name, "w")
     temp_file.write(mol2d)
     temp_file.close()
-    end = time.time()
-    output += "parse input: {} ".format(str(end-start))
-    start = time.time()
     supplier = Chem.SDMolSupplier(temp_file_name)
     mol = supplier[0]
     smiles = Chem.MolToSmiles(mol)
 
     sanitized_smiles = operations.test_source_smiles_convert_update_properties(smiles)
-    end = time.time()
-    output += "set up and sanitize: {} ".format(str(end-start))
-    start = time.time()
 
     # if smiles != sanitized_smiles:
     #     return "smiles did not sanitize"
@@ -83,14 +70,8 @@ def updateProperties():
     # the method calls will only use what they need
 
     vars = user_vars.define_defaults()
-    end = time.time()
-    output += "define defaults: {} ".format(str(end-start))
-    start = time.time()
     vars = user_vars.multiprocess_handling(vars)
-    end = time.time()
-    output += "multiprocess handling: {} ".format(str(end-start))
     vars["max_variants_per_compound"] = 1
-    start = time.time()
     smiles_list = [(sanitized_smiles, referred_mol_name)]
     smiles_to_convert_file, new_gen_folder_path = operations.save_generation_smi(
         os.getcwd()+"/smiles_dir/",
@@ -98,24 +79,11 @@ def updateProperties():
         smiles_list,
         None,
         )
-    end = time.time()
-    output += "save smi: {} ".format(str(end-start))
-    start = time.time()
     conversion_to_3d.convert_to_3d(vars, smiles_to_convert_file, new_gen_folder_path)
-    end = time.time()
-    output += "convert to 3d: {} ".format(str(end-start))
-    start = time.time()
     file_in_3d_folder = os.system("dir "+new_gen_folder_path+"/3D_SDFs/ > 3d_folder_contents.txt")
 
     rdkit_mol_sdf = Chem.MolToMolBlock(mol)
-    end = time.time()
-    output += "mol to mol block: {} ".format(str(end-start))
-    start = time.time()
 
-    folder_contents = open("3d_folder_contents.txt").read()
-    print(folder_contents)
-    total_end = time.time()
-    output += "total process time: {}".format(total_end-total_start)
     # return output
     threed_sdf = open(new_gen_folder_path+"/3D_SDFs/{}__input1.sdf".format(referred_mol_name)).read()
     os.remove(new_gen_folder_path+"/3D_SDFs/{}__input1.sdf".format(referred_mol_name))
